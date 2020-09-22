9c43aa2892cdd37bceffd090e2f4ffb298dfde93
kabirsky <kabirsky@yandex.ru>
2020-09-22 23:54:55 +0300

    Updates

---
+++ precache-manifest.2a36f27fedee6b7aad3855ea79b891e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4b5de7a00ce6b13412ac286672fdb0",
+    "url": "/greensight/index.html"
+  },
+  {
+    "revision": "85fd9802f81971a6ef3e",
+    "url": "/greensight/static/css/main.8aa14212.chunk.css"
+  },
+  {
+    "revision": "c6419e84dbcf109292cc",
+    "url": "/greensight/static/js/2.e3b7b7da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/greensight/static/js/2.e3b7b7da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85fd9802f81971a6ef3e",
+    "url": "/greensight/static/js/main.e0510eef.chunk.js"
+  },
+  {
+    "revision": "21230413ca60b8deeaa1",
+    "url": "/greensight/static/js/runtime-main.7557f0ce.js"
+  }
+]);
