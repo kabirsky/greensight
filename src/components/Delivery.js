1b33de5695e7bf6ae432d9c796d8e1c67f0c14ba
kabirsky <kabirsky@yandex.ru>
2020-09-25 13:16:38 +0300

    Delivery page complete

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -22,13 +22,80 @@ function Delivery() {
     /[0-9]/,
     /[0-9]/,
   ];
-
+  /*const isValid = {
+    nameExist: true,
+    isName: true,
+    phoneExist: true,
+    addressExist: true,
+  };*/
+  let isError = false;
   const [disabled, setDisabled] = useState(false);
+  const [name, setName] = useState(null);
+  const [phone, setPhone] = useState(null);
+  const [address, setAddress] = useState(null);
+  const [comment, setComment] = useState(null);
+
+  const [errorName, setErrorName] = useState("err__icon disable");
+  const [errorPhone, setErrorPhone] = useState("err__icon disable");
+  const [errorAddress, setErrorAddress] = useState("err__icon disable");
+
+  const [errorNameText, setErrorNameText] = useState(null);
+
+  const handleChangeName = (event) => {
+    setName(event.target.value);
+  };
+  const handleChangePhone = (event) => {
+    setPhone(event.target.value);
+  };
+  const handleChangeAddress = (event) => {
+    setAddress(event.target.value);
+  };
+  const handleChangeComment = (event) => {
+    setComment(event.target.value);
+  };
 
   const handleClickSubmit = (event) => {
-    setDisabled(true);
-    setTimeout(() => {alert("Заказ оформлен!");setDisabled(false);}, 1000);
-    
+    isError = false;
+
+    //Проверка ФИО
+    if (name === null || name === "") {
+      isError = true;
+      setErrorName("err__icon");
+      setErrorNameText("Введите имя");
+    } else {
+      if (name.match(/^[А-ЯЁ\s-]*$/i) === null) {
+        isError = true;
+        setErrorName("err__icon");
+        setErrorNameText("Некорректное имя");
+      } else {
+        setErrorName("err__icon disable");
+        setErrorNameText(null);
+      }
+    }
+
+    //Проверка телефона
+    if (phone === null || phone === "") {
+      isError = true;
+      setErrorPhone("err__icon");
+    } else {
+      setErrorPhone("err__icon disable");
+    }
+
+    //Проверка адреса
+    if (address === null || address === "") {
+      isError = true;
+      setErrorAddress("err__icon");
+    } else {
+      setErrorAddress("err__icon disable");
+    }
+
+    if (!isError) {
+      setDisabled(true);
+      setTimeout(() => {
+        alert("Заказ оформлен!");
+        setDisabled(false);
+      }, 1000);
+    }
   };
 
   return (
@@ -39,35 +106,57 @@ function Delivery() {
             <div className="form__element fio">
               <label className="label">
                 ФИО
-                <input className="input" placeholder="Только кириллица" />
+                <span className={errorName}></span>
+                <input
+                  className="input"
+                  onChange={handleChangeName}
+                  placeholder="Только кириллица"
+                />
               </label>
+              {errorName === "err__icon" ? (
+                <label className="label err">{errorNameText}</label>
+              ) : null}
             </div>
 
             <div className="form__element phone">
               <label className="label">
                 Телефон
+                <span className={errorPhone}></span>
                 <MaskedInput
                   className="input"
+                  onChange={handleChangePhone}
                   mask={mask}
                   placeholder="+7(___) ___-__-__"
                 />
               </label>
+              {errorPhone === "err__icon" ? (
+                <label className="label err">Введите телефон</label>
+              ) : null}
             </div>
 
             <div className="form__element address">
               <label className="label address__label">
                 Адрес доставки
+                <span className={errorAddress}></span>
                 <input
                   className="input address__input"
+                  onChange={handleChangeAddress}
                   placeholder="Город, улица, дом"
                 />
               </label>
+              {errorAddress === "err__icon" ? (
+                <label className="label err">Введите адрес</label>
+              ) : null}
             </div>
 
             <div className="form__element comment">
               <label className="label comment__label">
                 Комментарий
-                <textarea className="input comment__input" rows={4} />
+                <textarea
+                  className="input comment__input"
+                  onChange={handleChangeComment}
+                  rows={4}
+                />
               </label>
             </div>
           </div>
